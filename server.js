b196c3adbd51d59a0e30de67707a20bf1c949695
LukeT2340 <lukethompson220@gmail.com>
2024-05-05 20:50:00 +1000

    Revamped website layout to a single-page design with navigation bar linking to specific sections. Redesigned layout for improved visual appeal.

--- server.js
+++ server.js
@@ -7,27 +7,7 @@ const app = express()
 // Get app to use the static files in the /public directory
 app.use(express.static(__dirname + '/public'))
 
-// Home page
+// Website home page
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/public/index.html')
     })
-
-// About me page
-app.get('/about', (req, res) => {
-    res.sendFile(__dirname + '/public/about.html')
-})
-
-// Projects page
-app.get('/projects', (req, res) => {
-    res.sendFile(__dirname + '/public/projects.html')
-})
-
-// Contact page
-app.get('/contact', (req, res) => {
-    res.sendFile(__dirname + '/public/contact.html')
-})
-
-// Run server
-app.listen(80, ()=>{
-    console.log("Server running at http://127.0.0.1")
-})
