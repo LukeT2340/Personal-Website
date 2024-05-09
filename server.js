17b6b165b040dc7d760b4a28ab9372bd2526693a
LukeT2340 <lukethompson220@gmail.com>
2024-05-09 10:07:00 +1000

    Minor Changes

--- server.js
+++ server.js
@@ -9,5 +9,9 @@ app.use(express.static(__dirname + '/public'))
 
 // Website home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')
-    })
+    res.sendFile(__dirname + '/public/index.html')    
+})
+
+app.listen(80, () => {
+    console.log(`Server is running at http://127.0.0.1`);
+});
