0a78df08f771c4218a51916a0bd3e52813890886
LukeT2340 <lukethompson220@gmail.com>
2024-05-09 10:57:31 +1000

    Adjusted server.js to make website publicly accessible

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 // Import express module
-const express = require('express')
+const express = require('express');
+const path = require('path');
 
 // Create new app
-const app = express()
+const app = express();
 
 // Get app to use the static files in the /public directory
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Website home page
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/index.html')    
-})
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
-app.listen(80, () => {
-    console.log(`Server is running at http://127.0.0.1`);
+// Use process.env.PORT if it exists, otherwise use port 3000
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
